0989ad52f0d949d4ace1b80e216a0ccccd37e747
Bartek <74018739+Thalruk@users.noreply.github.com>
2026-07-20 16:30:55 +0200

    feat: Create note CRUD

---
+++ Backend/Controllers/NotesController.cs
@@ -0,0 +1,81 @@
+﻿using Backend.Models;
+using Chronote.Api.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public NotesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Note>>> GetNotes()
+        {
+            return await _context.Notes
+                .OrderByDescending(n => n.CreatedAt)
+                .ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Note>> GetNote(Guid id)
+        {
+            var note = await _context.Notes.FindAsync(id);
+
+            if (note == null)
+            {
+                return NotFound();
+            }
+
+            return note;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Note>> CreateNote(Note note)
+        {
+            note.Id = Guid.NewGuid();
+            note.CreatedAt = DateTime.UtcNow;
+
+            _context.Notes.Add(note);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetNote), new { id = note.Id }, note);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateNote(Guid id, Note updatedNote)
+        {
+            if (id != updatedNote.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(updatedNote).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteNote(Guid id)
+        {
+            var note = await _context.Notes.FindAsync(id);
+            if (note == null)
+            {
+                return NotFound();
+            }
+
+            _context.Notes.Remove(note);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
