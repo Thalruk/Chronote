9c83f58e581e9566285c2730b44657065ca7db99
Bartek <74018739+Thalruk@users.noreply.github.com>
2026-07-20 15:55:31 +0200

    feat: create Note entity and apply migration

--- Backend/ApplicationDbContext.cs
+++ Backend/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Backend.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Chronote.Api.Data;
 
@@ -6,6 +7,7 @@ public class ApplicationDbContext : DbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
-    {
-    }
+    {}
+    public DbSet<Note> Notes { get; set; }
+
 }
