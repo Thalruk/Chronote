f0c377c0617c639f90b3621e40a75b305f03ad06
Bartek <74018739+Thalruk@users.noreply.github.com>
2026-08-07 17:52:35 +0200

    Changed environment variables

---
+++ Frontend/src/environments/environment.production.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://chronote-backend.onrender.com/api',
+    googleClientId: '392616601934-7hn7j64fpkpkhtmjpvsonq52cmg6mo90.apps.googleusercontent.com',
+};
